ac794c980e6adcfd70391de0117a3a9385ec2edd
Ivan <ivansinohon28@gmail.com>
2025-01-27 18:01:34 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 "use client"
 import { useRouter } from "next/navigation"
+import { Input } from "@/components/ui/input"
+import { Button } from "react-day-picker"
 
 const Home = () => {
   const router = useRouter()
 
-  return router.push("/admin")
+  return (
+    <div className="h-screen w-screen flex justify-center items-center bg-primary">
+      <div className="flex flex-col h-fit w-full items-center bg-white">
+        <h1>Game Calculator</h1>
+        <Input className="w-full" placeholder="Enter your username" />
+        <Input className="w-full" placeholder="Enter your password" />
+        <Button onClick={() => router.push("/admin/sessions")}>Sign In</Button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
