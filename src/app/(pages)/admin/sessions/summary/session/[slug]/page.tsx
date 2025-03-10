c67d5e324a17b30193c92faf9891bc5715c100c3
PRINCE <princenagac12@gmail.com>
2025-03-11 05:52:58 +0800

    Fix import statement in page.tsx, remove extra newline in schemas.ts, and adjust button styles in GameForm for improved UI consistency

--- src/app/(pages)/admin/sessions/summary/session/[slug]/page.tsx
+++ src/app/(pages)/admin/sessions/summary/session/[slug]/page.tsx
@@ -1,9 +1,8 @@
-"use client"
-import { gql, useQuery } from "@apollo/client"
-import { useParams } from "next/navigation"
-// import { saveAs } from "file-saver"
-import { Separator } from "@/components/ui/separator"
-import { Loader2 } from "lucide-react"
+"use client";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import { Loader2 } from "lucide-react";
 
 const FETCH_SUMMARY = gql`
   query FetchSummary($id: ID!) {
@@ -18,6 +17,11 @@ const FETCH_SUMMARY = gql`
         name
         totalRate
       }
+      sponsorSummaryRates {
+        _id
+        name
+        totalRate
+      }
       durationPerCourt {
         totalDuration
         court {
@@ -29,91 +33,144 @@ const FETCH_SUMMARY = gql`
           updatedAt
         }
       }
+      session {
+        games {
+          A1 {
+            _id
+            name
+            sponsoredBy {
+              _id
+              name
+            }
+          }
+          A2 {
+            _id
+            name
+            sponsoredBy {
+              _id
+              name
+            }
+          }
+          B1 {
+            _id
+            name
+            sponsoredBy {
+              _id
+              name
+            }
+          }
+          B2 {
+            _id
+            name
+            sponsoredBy {
+              _id
+              name
+            }
+          }
+        }
+      }
     }
   }
 `
 
 const Page = () => {
-  const { slug } = useParams()
+  const { slug } = useParams();
   const { data, loading, error } = useQuery(FETCH_SUMMARY, {
     ssr: false,
     skip: !slug,
     variables: { id: slug },
     fetchPolicy: "network-only",
-  })
+  });
+
   if (data) {
-    console.log("Fetched User Data:", data.fetchSessionSummary)
+    console.log("Fetched User Data:", data.fetchSessionSummary);
   }
-  
+
   const formatNumberWithCommas = (number: number) => {
     return number.toLocaleString(undefined, {
       minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    })
-  }
+      maximumFractionDigits: 2,
+    });
+  };
 
-  // Sort the name to A-Z
-  const sortedPlayerSummaryRates = data?.fetchSessionSummary?.playerSummaryRates ?
-  [...data.fetchSessionSummary.playerSummaryRates].sort((a, b) => a.name.localeCompare(b.name)) : []
+  const sortedPlayerSummaryRates = data?.fetchSessionSummary?.playerSummaryRates
+    ? [...data.fetchSessionSummary.playerSummaryRates].sort((a, b) =>
+        a.name.localeCompare(b.name)
+      )
+    : [];
 
-  // useEffect(() => {
-  //   if (data?.fetchSessionSummary) {
-  //     exportToCSV(data.fetchSessionSummary)
-  //   }
-  // }, [data])
+  const isPlayerSponsored = (playerId: string) => {
+    const games = data?.fetchSessionSummary?.session?.games || [];
+    for (const game of games) {
+      const players = [game.A1, game.A2, game.B1, game.B2];
+      for (const player of players) {
+        if (player?._id === playerId && player?.sponsoredBy?.length > 0) {
+          return true; 
+        }
+      }
+    }
+    return false; 
+  };
 
-  // HERE IS COMMENTED
-  // const exportToCSV = (summary: any) => {
-  //   const today = new Date().toISOString().split("T")[0] 
-  //   const filename = `session_summary_${today}.csv`
-  //   let csvContent = "Category,Value\n"
+  const getPlayersSponsoredBy = (sponsorId: string) => {
+    const games = data?.fetchSessionSummary?.session?.games || [];
+    const sponsoredPlayers: { name: string; _id: string }[] = [];
 
-  //   csvContent += `ID,${slug}\n`
-  //   csvContent += `Total Shuttles Used,${summary.totalShuttlesUsed}\n`
-  //   csvContent += `Court Total,${summary.courtTotal.toFixed(2)}\n`
-  //   csvContent += `Shuttle Total,${summary.shuttleTotal.toFixed(2)}\n`
-  //   csvContent += `Player Total,${summary.playerTotal.toFixed(2)}\n\n`
+    for (const game of games) {
+      const players = [game.A1, game.A2, game.B1, game.B2];
+      for (const player of players) {
+        if (
+          player?.sponsoredBy?.some((sponsor: any) => sponsor._id === sponsorId)
+        ) {
+          sponsoredPlayers.push({ name: player.name, _id: player._id });
+        }
+      }
+    }
 
-  //   csvContent += "Court Name,Total Duration (mins)\n"
-  //   summary.durationPerCourt.forEach((court: any) => {
-  //     csvContent += `${court.court.name},${court.totalDuration}\n`
-  //   })
+    return sponsoredPlayers;
+  };
 
-  //   csvContent += "\nPlayer Name,Total Rate\n"
-  //   summary.playerSummaryRates.forEach((player: any) => {
-  //     csvContent += `${player.name},${player.totalRate.toFixed(2)}\n`
-  //   })
+  // Function to get sponsor rate by sponsor ID
+  const getSponsorRate = (sponsorId: string) => {
+    const sponsor = data?.fetchSessionSummary?.sponsorSummaryRates?.find(
+      (s: any) => s._id === sponsorId
+    );
+    return sponsor ? sponsor.totalRate : 0;
+  };
 
-  //   const blob = new Blob(["\ufeff" + csvContent], { type: "text/csv;charset=utf-8;" })
-  //   saveAs(blob, filename)
-  // }
-
-  if (loading) return <div className="flex-1 h-fit flex items-center justify-center"> <Loader2 className="animate-spin" size={200} /></div>
+  if (loading)
+    return (
+      <div className="flex-1 h-fit flex items-center justify-center">
+        <Loader2 className="animate-spin" size={200} />
+      </div>
+    );
   if (error) {
-    console.error("GraphQL Error:", error)
-    return <div>Error fetching Summary.</div>
+    console.error("GraphQL Error:", error);
+    return <div>Error fetching Summary.</div>;
   }
 
-  const summary = data?.fetchSessionSummary
+  const summary = data?.fetchSessionSummary;
 
   return (
     <div className="h-fit flex-1 overflow-auto w-full flex flex-col gap-2 p-2">
       <div>
         <span className="block font-semibold text-muted-foreground">ID</span>
-        <span className="block font-bold text-muted-foreground">
-          {slug}
-        </span>
+        <span className="block font-bold text-muted-foreground">{slug}</span>
       </div>
       {summary?.durationPerCourt.map((court: any) => (
         <div key={court.court._id}>
-          <span className="block font-semibold text-muted-foreground">Court Duration</span>
+          <span className="block font-semibold text-muted-foreground">
+            Court Duration
+          </span>
           <span className="block font-bold text-muted-foreground">
             {court.court.name} - {court.totalDuration}mins
           </span>
         </div>
       ))}
       <div>
-        <span className="block font-semibold text-muted-foreground">Shuttles Used</span>
+        <span className="block font-semibold text-muted-foreground">
+          Shuttles Used
+        </span>
         <span className="block font-bold text-muted-foreground">
           {summary?.totalShuttlesUsed} shuttles
         </span>
@@ -121,19 +178,25 @@ const Page = () => {
       <Separator className="bg-slate-400" />
       <div className="grid grid-cols-2">
         <div>
-          <span className="block font-semibold text-muted-foreground">Court Total</span>
+          <span className="block font-semibold text-muted-foreground">
+            Court Total
+          </span>
           <span className="block font-bold text-muted-foreground">
             {formatNumberWithCommas(summary?.courtTotal)}
           </span>
         </div>
         <div>
-          <span className="block font-semibold text-muted-foreground">Shuttle Total</span>
+          <span className="block font-semibold text-muted-foreground">
+            Shuttle Total
+          </span>
           <span className="block font-bold text-muted-foreground">
-          {formatNumberWithCommas(summary?.shuttleTotal)}
+            {formatNumberWithCommas(summary?.shuttleTotal)}
           </span>
         </div>
         <div>
-          <span className="block font-semibold text-muted-foreground">Player Total</span>
+          <span className="block font-semibold text-muted-foreground">
+            Player Total
+          </span>
           <span className="block font-bold text-muted-foreground">
             {formatNumberWithCommas(summary?.playerTotal)}
           </span>
@@ -142,14 +205,37 @@ const Page = () => {
 
       <Separator className="bg-slate-400" />
       <div className="grid grid-cols-2">
-        {sortedPlayerSummaryRates.map((player: any) => (
-          <div key={player._id}>
-            <span className="block font-semibold text-muted-foreground">{player.name}</span>
-            <span className="block font-bold text-muted-foreground">
-              {formatNumberWithCommas(player.totalRate)}
-            </span>
-          </div>
-        ))}
+        {sortedPlayerSummaryRates
+          .filter((player) => !isPlayerSponsored(player._id)) // Filter out sponsored players
+          .map((player: any) => {
+            const sponsoredPlayers = getPlayersSponsoredBy(player._id);
+            return (
+              <div key={player._id}>
+                <span className="block font-semibold text-muted-foreground">
+                  {player.name}
+                </span>
+                <span className="block font-bold text-muted-foreground">
+                  {formatNumberWithCommas(player.totalRate)}
+                </span>
+                {sponsoredPlayers.length > 0 && (
+                  <div className="block text-sm text-muted-foreground">
+                    <span>Sponsors:</span>
+                    {sponsoredPlayers.map((p) => {
+                      const sponsorRate = getSponsorRate(player._id);
+                      return (
+                        <div key={p._id}>
+                          <span>{p.name}</span>
+                          <span className="ml-2">
+                            (Rate: {formatNumberWithCommas(sponsorRate)})
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            )
+          })}
       </div>
     </div>
   )
